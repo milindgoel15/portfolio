adf9d8c33143d2dfb7f349be602df961194284fb
Milind Goel <milindgoel15@gmail.com>
2022-05-25 12:52:24 +0530

    add missing head info

--- pages/skills.js
+++ pages/skills.js
@@ -1,12 +1,19 @@
+import Head from 'next/head'
 import SkillCard from "../components/layouts/partials/SkillCard";
 import SkillsData from "../utils/SkillsData";
 import { motion } from 'framer-motion';
 
 let skills = () => {
+
    return (
       <>
+         <Head>
+            <title>Skills</title>
+            <meta name="title" content="Skills" />
+            <meta name="description" content="Building beautiful web experiences for various businesses with coffee and code." />
+         </Head>
          <main className="py-20 text-center">
-            <motion.h2 initial={{ opacity: 0, translateY: '30px' }} whileInView={{ opacity: 1, translateY: 0, transitionDuration: 0.5  }} viewport={{ once: true }} transition={{ duration: 0.5 }} className="font-extrabold text-4xl py-1 xl:mx-110 2xl:mx-128 text-black dark:text-white heading">Skills</motion.h2>
+            <motion.h2 initial={{ opacity: 0, translateY: '30px' }} whileInView={{ opacity: 1, translateY: 0, transitionDuration: 0.5 }} viewport={{ once: true }} transition={{ duration: 0.5 }} className="font-extrabold text-4xl py-1 xl:mx-110 2xl:mx-128 text-black dark:text-white heading">Skills</motion.h2>
 
             <section className="flex justify-center">
 
@@ -14,7 +21,7 @@ let skills = () => {
 
                   {
                      SkillsData.map(props => (
-                        <SkillCard 
+                        <SkillCard
                            key={props.id}
                            head={props.head}
                            lang={props.lang}
