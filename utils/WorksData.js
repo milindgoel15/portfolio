1fa51900ad3c17ee5691d63f6563bd1bf90ef318
Milind Goel <milindgoel15@gmail.com>
2022-10-17 10:09:03 +0530

    update work data

--- utils/WorksData.js
+++ utils/WorksData.js
@@ -1,46 +1,46 @@
 let WorksData = [
    {
       id: 1,
+      title: 'Retro Weather',
+      desc: 'Material Design live-weather app',
+      lang: 'Flutter',
+      link: 'https://telegram.me/RetroWeather'
+   },
+   {
+      id: 2,
       title: 'Personal Portfolio - V2.1',
       desc: 'Designed personal Portfolio website for myself',
       lang: 'Next.js & TailwindCSS',
       link: 'https://github.com/milindgoel15/portfolio'
    },
    {
-      id: 2,
+      id: 3,
       title: 'Abhishek Goel & Associates',
       desc: 'Website for a Chartered Accountant firm',
       lang: 'React.js & TailwindCSS',
       link: 'https://abhishekgoelandassociates.com'
    },
    {
-      id: 3,
+      id: 4,
       title: 'Retro Music Player',
       desc: 'Member and contributor of Retro Music Player, a material design, offline music player for Android.',
       lang: 'Kotlin, Java, HTML5',
       link: 'https://github.com/RetroMusicPlayer'
    },
    {
-      id: 4,
+      id: 5,
       title: 'Milind Webcomm Services',
       desc: 'Website for Web Designing and E-Commerce operators',
       lang: 'TailwindCSS & AlpineJS',
       link: 'https://mwsretail.in'
    },
    {
-      id: 5,
-      title: 'Netflix Clone',
-      desc: 'A simple Frontend Netflix clone with slightly improved styling',
-      lang: 'React.js and TailwindCSS',
-      link: 'https://github.com/milindgoel15/netflix-clone'
-   },
-   {
       id: 6,
-      title: 'Microsoft Clone',
-      desc: 'Built a Frontend Microsoft clone using ReactJS and TailwindCSS',
-      lang: 'React.js and TailwindCSS',
-      link: 'https://github.com/milindgoel15/microsoft-clone'
-   }
+      title: 'Quiz Trivia',
+      desc: 'Quiz Trivia based on Trivia API',
+      lang: 'Next.js, TailwindCSS & Open Trivia API',
+      link: 'https://github.com/milindgoel15/quiztrivia'
+   },
 ]
 
 export default WorksData;
