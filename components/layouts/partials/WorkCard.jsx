041530939a9659f073e7871e8d791a91ba90ef79
Milind Goel <milindgoel15@gmail.com>
2023-02-08 12:57:12 +0530

    fix work card height

--- components/layouts/partials/WorkCard.jsx
+++ components/layouts/partials/WorkCard.jsx
@@ -5,33 +5,31 @@ import LinkButton from "../../elements/link";
 let WorkCard = (props) => {
   return (
     <>
-      <a key={props.id} href={props.link} target="_blank" rel="noreferrer">
-        <motion.div
-          initial={{ opacity: 0, translateY: "10px" }}
-          whileInView={{ opacity: 1, translateY: 0 }}
-          viewport={{ once: true }}
-          transition={{
-            duration: 0.3,
-            delay: `${props.i}` * 0.05,
-            ease: "easeIn",
-          }}
-          className="py-4 sm:py-8 px-6 sm:h-80 sm:px-10 md:px-8 h-[17rem] bg-blue-600 hover:bg-blue-700 border-transparent rounded-md shadow-md space-y-2 mt-5 ml-4 w-11/12 sm:ml-0 sm:w-80 md:w-96 sm:mt-0 md:h-72 lg:h-72 lg:w-[30rem] text-left relative transtion-all duration-300 ease-in-out top-0 hover:-top-1 hover:shadow-xl group foldWorkCard"
-        >
-          <div className="flex justify-between items-center">
-            <h2 className="text-2xl py-5 text-white font-semibold leading-6">
-              {props.title}
-            </h2>
-            <div className="lg:invisible lg:group-hover:visible ">
-              <LinkButton />
-            </div>
+      <motion.a key={props.id} href={props.link} target="_blank" rel="noreferrer"
+        initial={{ opacity: 0, translateY: "10px" }}
+        whileInView={{ opacity: 1, translateY: 0 }}
+        viewport={{ once: true }}
+        transition={{
+          duration: 0.3,
+          delay: `${props.i}` * 0.05,
+          ease: "easeIn",
+        }}
+        className="py-4 sm:py-8 px-6 sm:h-80 sm:px-10 md:px-8 min-h-fit bg-blue-600 hover:bg-blue-700 border-transparent rounded-md shadow-md space-y-2 mt-5 ml-4 w-11/12 sm:ml-0 sm:w-80 md:w-96 sm:mt-0 md:h-72 lg:h-72 lg:w-[30rem] text-left relative transtion-all duration-300 ease-in-out top-0 hover:-top-1 hover:shadow-xl group foldWorkCard"
+      >
+        <div className="flex justify-between items-center">
+          <h2 className="text-2xl py-5 text-white font-semibold leading-6">
+            {props.title}
+          </h2>
+          <div className="lg:invisible lg:group-hover:visible ">
+            <LinkButton />
           </div>
-          <p className="text-lg text-slate-200">{props.desc}</p>
-          <p className="text-lg text-slate-200">{props.role}</p>
-          <p className="text-lg text-slate-200 py-1 lg:py-2 w-11/12">
-            {props.lang}
-          </p>
-        </motion.div>
-      </a>
+        </div>
+        <p className="text-lg text-slate-200">{props.desc}</p>
+        <p className="text-lg text-slate-200">{props.role}</p>
+        <p className="text-lg text-slate-200 py-1 lg:py-2 w-11/12">
+          {props.lang}
+        </p>
+      </motion.a>
     </>
   );
 };
